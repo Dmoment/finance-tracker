752422dfb0a5bf3b569f6b72904aee092217266b
DEEPAK CHAUHAN <rishudc119@gmail.com>
2020-06-07 10:06:31 +0530

    Stock tracking restrictions

--- app/controllers/user_stocks_controller.rb
+++ app/controllers/user_stocks_controller.rb
@@ -1,2 +1,13 @@
 class UserStocksController < ApplicationController
+    def create
+         stock= Stock.check_db(params[:ticker])
+         if !stock
+            stock= Stock.new_lookup(params[:ticker])
+            stock.save #As soon as this will hit the database i.e. stocktable an id will be generated of the record
+         end
+            @user_stock= UserStock.create(user: current_user, stock: stock)#In the stock entry we will put the above id through passing stock object
+            flash[:success]="Your stock #{stock.name} is added successfully"
+            redirect_to my_portfolio_path
+         
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,21 @@ class User < ApplicationRecord
   has_many :stocks, through: :user_stocks
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+         
+  def stocks_already_tracked?(ticker_symbol)
+    stock= Stock.check_db(ticker_symbol) #Ensures whether the stock is already there in database
+    return false unless stock
+    stocks.where(id: stock.id).exists?# Ensures whether the current user is associated with this stock.
+
+  end
+
+  def stock_under_limit?
+    stocks.count < 10
+  end
+
+  def can_track_stock?(ticker_symbol)
+    stock_under_limit? && !stocks_already_tracked?(ticker_symbol)
+  end
+        
 end
