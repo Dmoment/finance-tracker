8e15b4362d2f476ceb86ae88986b82856fcd001c
DEEPAK CHAUHAN <rishudc119@gmail.com>
2020-06-10 17:21:30 +0530

    Customize devise to add new attributes

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,8 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+require('jquery')
+require("jquery-ui")
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
@@ -16,3 +18,8 @@ require("channels")
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
 import "bootstrap"
+
+$( document ).on('turbolinks:load', function() {
+    console.log("It works on each visit!")
+  })
+  
