f997df07a1ea3546b2fcbc232127cf0cd7f34869
DEEPAK CHAUHAN <rishudc119@gmail.com>
2020-06-28 13:59:55 +0530

    prod testing 2

--- app/controllers/user_stocks_controller.rb
+++ app/controllers/user_stocks_controller.rb
@@ -6,7 +6,7 @@ class UserStocksController < ApplicationController
          stock.save #As soon as this will hit the database i.e. stocktable an id will be generated of the record
       end
          @user_stock= UserStock.create(user: current_user, stock: stock)#In the stock entry we will put the above id through passing stock object
-         flash[:success]="Your stock #{stock.name} is added successfully"
+         flash[:notice]="Your stock #{stock.name} is added successfully"
          redirect_to my_portfolio_path
          
     end
