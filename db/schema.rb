4b0ff4e367d60be792303a1919c5293508cfb076
DEEPAK CHAUHAN <rishudc119@gmail.com>
2020-07-18 21:58:13 +0530

    testing

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_07_104144) do
+ActiveRecord::Schema.define(version: 2020_07_18_154811) do
 
   create_table "friendships", force: :cascade do |t|
     t.integer "user_id", null: false
@@ -52,7 +52,6 @@ ActiveRecord::Schema.define(version: 2020_07_07_104144) do
     t.string "confirmation_token"
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
-    t.string "unconfirmed_email"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
