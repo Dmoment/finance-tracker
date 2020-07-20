888f608f14484140e135f38bb7a1e79c076d8df6
DEEPAK CHAUHAN <rishudc119@gmail.com>
2020-07-20 07:14:34 +0530

    testing app for prod

--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,13 @@ Rails.application.routes.draw do
   resources :friendships, only: [:create,:destroy]
   root to:'welcome#index'
   devise_for :users, controllers: { confirmations: 'confirmations' }
+  
+  resources :users, only: [:show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   
   get 'my_portfolio', to: 'users#my_portfolio'
   get 'search_stocks', to: 'stocks#search'
   get 'my_friends', to: 'users#my_friends'
   get 'search_friends', to: 'users#search'
-  resources :users, only: [:show]
+  
 end
